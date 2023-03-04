325703626d3ddb2bf7e37c01bab475f6d49e196a
farankinstan <89528308+farankinstan@users.noreply.github.com>
2023-03-04 20:16:27 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.L_path = new System.Windows.Forms.Label();
+            this.L_state = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(204, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 67);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // L_path
+            // 
+            this.L_path.AutoSize = true;
+            this.L_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.L_path.Location = new System.Drawing.Point(13, 13);
+            this.L_path.MaximumSize = new System.Drawing.Size(388, 0);
+            this.L_path.Name = "L_path";
+            this.L_path.Size = new System.Drawing.Size(38, 18);
+            this.L_path.TabIndex = 1;
+            this.L_path.Text = "Path";
+            // 
+            // L_state
+            // 
+            this.L_state.AutoSize = true;
+            this.L_state.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.L_state.ForeColor = System.Drawing.Color.Red;
+            this.L_state.Location = new System.Drawing.Point(13, 65);
+            this.L_state.Name = "L_state";
+            this.L_state.Size = new System.Drawing.Size(41, 18);
+            this.L_state.TabIndex = 1;
+            this.L_state.Text = "OFF";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormatString = "N0";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 169);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Tag = "";
+            this.comboBox1.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Duration";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(144, 169);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 24);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(16, 120);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "HotKey";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 290);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.L_state);
+            this.Controls.Add(this.L_path);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lag Switch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label L_path;
+        private System.Windows.Forms.Label L_state;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        private static extern bool GetAsyncKeyState(ushort vKey);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //L_hotkey.Text = VK.VirtualKeys.CapsLock.ToString();
+            comboBox2.DataSource = Enum.GetValues(typeof(VK.VirtualKeys));
+            timer1.Enabled= false;
+        }
+
+
+        bool active = false;
+        string gamepath = "";
+        ushort hotkey;
+        int duration = -1;
+
+        ProcessStartInfo AddRuleIn;
+        ProcessStartInfo AddRuleOut;
+        ProcessStartInfo DeleteRule;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkBox1.Checked) { 
+                duration = int.Parse(comboBox1.Text);
+                }
+            }
+            catch { 
+                checkBox1.Checked = false;
+                comboBox1.Enabled = false;
+                comboBox1.Text = "5";
+                duration = -1;
+
+                MessageBox.Show("ON\\OFF mode turned on", "No duration", MessageBoxButtons.OK);
+            }
+
+            hotkey = (ushort)Enum.Parse(typeof(VK.VirtualKeys), comboBox2.SelectedItem.ToString());
+
+            if(gamepath.Length == 0)
+            {
+                MessageBox.Show("Choose the path of you'r game", "No path", MessageBoxButtons.OK);
+                return;
+            }
+
+            AddRuleIn = new ProcessStartInfo("cmd.exe", "/c netsh advfirewall firewall add rule name=\"UCLagSwitch\" dir=in action=block program=\"" + gamepath + "\" enable=yes");
+            AddRuleOut = new ProcessStartInfo("cmd.exe", "/c netsh advfirewall firewall add rule name=\"UCLagSwitch\" dir=out action=block program=\"" + gamepath + "\" enable=yes");
+            DeleteRule = new ProcessStartInfo("cmd.exe", "/c netsh advfirewall firewall delete rule name=\"UCLagSwitch\" program=\"" + gamepath + "\"");
+
+            AddRuleIn.WindowStyle = ProcessWindowStyle.Hidden;
+            AddRuleOut.WindowStyle = ProcessWindowStyle.Hidden;
+            DeleteRule.WindowStyle = ProcessWindowStyle.Hidden;
+
+            timer1.Enabled = true;
+            MessageBox.Show("Enjoy you'r lag, sir", "Saved!", MessageBoxButtons.OK);
+        }
+
+        void OnOff()
+        {
+            if (active) {
+                L_state.Text = "ON";
+                L_state.ForeColor = Color.Green;
+            }
+
+            else
+            {
+                L_state.Text = "OFF";
+                L_state.ForeColor = Color.Red;
+            }
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            if (GetAsyncKeyState(hotkey) && duration > 0)
+            {
+                Process.Start(AddRuleIn);
+                Process.Start(AddRuleOut);
+                Console.Beep();
+                active = true;
+                OnOff();
+
+                await Task.Delay(duration * 1000);
+
+                Console.Beep();
+                active = false;
+                OnOff();
+                Process.Start(DeleteRule);
+            }
+
+            else if(GetAsyncKeyState(hotkey))
+            {
+                active = !active;
+
+                if (active)
+                {
+                    OnOff();
+                    Process.Start(AddRuleIn);
+                    Process.Start(AddRuleOut);
+                    Console.Beep();
+                }
+
+                else
+                {
+                    OnOff();
+                    Console.Beep();
+                    Process.Start(DeleteRule);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog selectExecutable = new OpenFileDialog())
+            {
+                //selectExecutable.Filter = ""; 
+
+                if (selectExecutable.ShowDialog() == DialogResult.OK)
+                {
+                    gamepath = L_path.Text = selectExecutable.FileName;
+                }
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+                duration = -1;
+            }
+
+            comboBox1.Enabled = checkBox1.Checked;
+        }
+    }
+}
